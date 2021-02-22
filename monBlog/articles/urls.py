7ed9ad06a39c402ff31ab6e5b25241832670a65c
Sevket73 <sevketsalkim73@gmail.com>
2021-02-22 09:16:15 +0100

    Suppression des dossiers inutiles 2

--- monBlog/articles/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path, include
-from .  import views
-
-urlpatterns = [
-    path('', views.articles_ind, name='articles_ind'),
-    path('<str:name>', include('article.urls'),name="article"),
-
-]
--- monBlog/articles/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def articles_ind(request):
-    return render(request,'articles/articles_ind.html')
-
-#def article(request, name):
-#    return render(request, 'monActivite/article.html')
